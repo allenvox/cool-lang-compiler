4b1989d54e8ff8fd42ec209815a97b30078b2b86
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-02-01 23:20:08 +0700

    Add example file for testing integer literals

---
+++ regex/example.cpp
@@ -0,0 +1,18 @@
+// decimal
+int d1 = 1023;
+unsigned int d2 = 3;
+long int d3 = 1;
+long long d4 = 2;
+unsigned long long d5 = 100'500;
+unsigned long d6 = 11249102uL;
+
+// oct
+int o = 0553;
+
+// hex
+int h1 = 0x5A;
+int h2 = 0X998a;
+
+// byte
+int b1 = 0b10101;
+int b2 = 0B00001;
