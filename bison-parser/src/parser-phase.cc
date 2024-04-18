4485d974c09f5129ed33fa2c7789d86f181b6704
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-04-19 00:19:45 +0700

    Implement error & sequence_out functions in namespace semantic

--- bison-parser/src/parser-phase.cc
+++ bison-parser/src/parser-phase.cc
@@ -21,6 +21,22 @@ int lex_verbose = 0;
 
 extern int cool_yyparse();
 
+namespace semantic {
+
+void error(std::string error_msg) {
+  std::cerr << "\tsemantic error: " << error_msg << '\n';
+}
+
+void sequence_out(std::string title, std::set<std::string> set) {
+  std::cerr << title << ": ";
+  for(auto s : set) {
+    std::cerr << s << ' ';
+  }
+  std::cerr << '\n';
+}
+
+}; // namespace semantic
+
 int main(int argc, char **argv) {
   yy_flex_debug = 0;
   cool_yydebug = 0;
@@ -41,6 +57,31 @@ int main(int argc, char **argv) {
     }
 
     /*
+    Symbol filename = stringtab.add_string("<builtin-classes>");
+    Symbol Object = idtable.add_string("Object");
+    Class_ Object_class = class_(
+      Object,
+      No_class,
+      append_Features(
+        append_Features(
+          single_Features(method(cool_abort, nil_Formals(), Object, no_expr())),
+          single_Features(method(type_name, nil_Formals(), Str, no_expr()))
+        ),
+        single_Features(method(copy, nil_Formals(), SELF_TYPE, no_expr()))
+      ),
+      filename
+    );*/
+
+    /* TODO
+     * 1. Предварительно добавить в таблицу символов такие имена как:
+     *    Main, main, Object, SELF_TYPE, ..., self, String, Int, Bool
+     * 2. Создать вектор с именами всех классов
+     * 3. Добавить метод GetParent в класс class__class
+     * 4. Добавить в список классов (AST) builtin-классы: Int, String, ...
+     * 5. Создать таблицу методов, для каждого класса
+     */
+
+    /*
     ast_root->dump_with_types(std::cerr, 0);
     std::cerr << "# Identifiers:\n";
     idtable.print();
@@ -52,19 +93,18 @@ int main(int argc, char **argv) {
 
     std::set<std::string> classes_names;
     GetName v;
-    for(int i = parse_results->first(); parse_results->more(i); i = parse_results->next(i)) {
+    for (int i = parse_results->first(); parse_results->more(i); i = parse_results->next(i)) {
       parse_results->nth(i)->accept(v);
+      if (strcmp(v.name, "SELF_TYPE") == 0) {
+        semantic::error("SELF_TYPE redeclared!");
+      }
       auto result = classes_names.insert(v.name);
       if(!result.second) {
-        std::cerr << "semantic: class " << v.name << " already exists\n";
+        semantic::error("class " + std::string(v.name) + " already exists!");
       }
     }
 
-    std::cerr << "Classes:\n";
-    for(auto s : classes_names) {
-      std::cerr << s << ' ';
-    }
-    std::cerr << '\n';
+    semantic::sequence_out("Classes", classes_names);
 
     std::fclose(token_file);
   }
