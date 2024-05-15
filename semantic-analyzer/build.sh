fb5c77ce9811a666dfeef7d0cb84df3c75a46f0e
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-05-15 13:09:34 +0700

    Change output executable name to analyzer

--- semantic-analyzer/build.sh
+++ semantic-analyzer/build.sh
@@ -4,4 +4,4 @@ CXXFLAGS="-Wall -Isrc/ -Iobj/ -Wno-unused -Wno-deprecated -Wno-write-strings -Wn
 
 bison -d -v -y -b cool --debug -p cool_yy -o obj/cool-bison-parser.cc src/cool.bison
 flex -d -o obj/cool-flex-lexer.cc src/cool.flex
-g++ -g $LDFLAGS $CXXFLAGS src/parser-phase.cc src/utilities.cc src/stringtab.cc src/cool-tree.cc obj/cool-flex-lexer.cc obj/cool-bison-parser.cc -o bin/parser
+g++ -g $LDFLAGS $CXXFLAGS src/parser-phase.cc src/utilities.cc src/stringtab.cc src/cool-tree.cc obj/cool-flex-lexer.cc obj/cool-bison-parser.cc -o bin/analyzer
