f261c4d98225d03d179bdb1fc91a250302d24370
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-05-22 20:35:11 +0700

    Modify semantic build script to make bin & obj directories, mute some steps

--- semantic-analyzer/build.sh
+++ semantic-analyzer/build.sh
@@ -2,6 +2,8 @@
 
 CXXFLAGS="-Wall -Isrc/ -Iobj/ -Wno-unused -Wno-deprecated -Wno-write-strings -Wno-free-nonheap-object"
 
+mkdir bin &> /dev/null
+mkdir obj &> /dev/null
 bison -d -v -y -b cool --debug -p cool_yy -o obj/cool-bison-parser.cc src/cool.bison
-flex -d -o obj/cool-flex-lexer.cc src/cool.flex
+flex -d -o obj/cool-flex-lexer.cc src/cool.flex &> /dev/null
 g++ -g $LDFLAGS $CXXFLAGS src/semantic-phase.cc src/utilities.cc src/stringtab.cc src/cool-tree.cc obj/cool-flex-lexer.cc obj/cool-bison-parser.cc -o bin/analyzer
