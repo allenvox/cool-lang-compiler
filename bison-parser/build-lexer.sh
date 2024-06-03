91e4b593fa924d5caf60e60e6c67ca2bc5ded919
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-06-03 20:39:51 +0700

    Modify lexer build script to create bin & obj directories

--- bison-parser/build-lexer.sh
+++ bison-parser/build-lexer.sh
@@ -2,5 +2,8 @@
 
 CXXFLAGS="-Wall -Isrc/ -Wno-unused -Wno-deprecated -Wno-write-strings -Wno-free-nonheap-object"
 
+mkdir bin &> /dev/null
+mkdir obj &> /dev/null
+
 flex -d -o obj/cool-flex-lexer.cc src/cool.flex
 g++ $CXXFLAGS src/utilities.cc src/stringtab.cc obj/cool-flex-lexer.cc src/lexer-test.cc -o bin/lexer
