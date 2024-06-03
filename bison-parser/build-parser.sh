d4a3d56ce6c126ae945ea4053ed1cfed5fd4b891
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2024-06-03 20:43:17 +0700

    Add mkdirs to build-parser script

--- bison-parser/build-parser.sh
+++ bison-parser/build-parser.sh
@@ -2,6 +2,9 @@
 
 CXXFLAGS="-Wall -Isrc/ -Iobj/ -Wno-unused -Wno-deprecated -Wno-write-strings -Wno-free-nonheap-object"
 
+mkdir bin &> /dev/null
+mkdir obj &> /dev/null
+
 bison -d -v -y -b cool --debug -p cool_yy -o obj/cool-bison-parser.cc src/cool.bison
 flex -d -o obj/cool-flex-lexer.cc src/cool.flex
 g++ -g $LDFLAGS $CXXFLAGS src/parser-phase.cc src/utilities.cc src/stringtab.cc src/cool-tree.cc obj/cool-flex-lexer.cc obj/cool-bison-parser.cc -o bin/parser
